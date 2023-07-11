ff9f86328b2c03467a258a8134f4a305a30972e1
codewithfaizan <faizanoic.com@gmail.com>
2023-07-12 01:42:36 +0530

    name changed, added versions

--- crossfit-wod-api/src/index.js
+++ crossfit-wod-api/src/index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req,res)=>{
-    res.send("<h1>Working Fine</h1>")
-});
+const v1Router = require("./v1/routes")
+
+app.use("/api/v1", v1Router)
 
 app.listen(PORT,()=>{
     console.log("Api is listening on Port", PORT)
