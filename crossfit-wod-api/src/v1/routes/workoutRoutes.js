e2d72289752b2b5118b279ef06b8f39d924107be
codewithfaizan <faizanoic.com@gmail.com>
2023-07-22 10:09:55 +0530

    define endpoints in workout router

--- crossfit-wod-api/src/v1/routes/workoutRoutes.js
+++ crossfit-wod-api/src/v1/routes/workoutRoutes.js
@@ -2,20 +2,21 @@
 const express = require("express")
 const router = express.Router();
 
+// This is how we define our end points in the workout router
 router.get("/", (req, res)=>{
  res.send("GET all workouts")
 });
 
 router.get("/:workoutId",(req, res)=>{
     res.send("GET an existing workout")
-})
+});
 router.post("/", (req, res)=> {
     res.send("Create a new workout")
-})
+});
 
 router.patch("/:workoutId", (req, res) =>{
 res.send("Update an existing workout")
-})
+});
 
 router.delete("/:workoutId", (req, res) =>{
     res.send("Delete an existing workout")
